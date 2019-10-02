f342537314517be59df71ac093824c347280fbd9
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-10-02 16:17:35 +0900

    Step1:complete

--- __tests__/step1.test.ts
+++ __tests__/step1.test.ts
@@ -1,13 +1,13 @@
 import { step1 } from '../src/step1';
 
 test('単語ひとつ', () => {
-  expect(step1('hello')).toMatchObject({ hello: 5 });
+  expect(step1('hello')).toMatchObject({ hello: 1 });
 });
 
 test('単語ふたつ', () => {
-  expect(step1('hello world')).toMatchObject({ hello: 5, world: 5 });
+  expect(step1('hello world hello')).toMatchObject({ hello: 2, world: 1 });
 });
 
 test('漢字', () => {
-  expect(step1('尾崎 勇一')).toMatchObject({ 尾崎: 2, 勇一: 2 });
+  expect(step1('尾崎 勇一')).toMatchObject({ 尾崎: 1, 勇一: 1 });
 });
--- src/step1.ts
+++ src/step1.ts
@@ -4,20 +4,7 @@ import readInput from './readInput';
 export function step1(value: string) {
   const result: { [key: string]: number } = {};
   value.split(' ').forEach((val: string) => {
-    result[val] = val.length;
+    result[val] = result[val] ? result[val] + 1 : 1;
   });
   return result;
 }
-
-(async () => {
-  let inputNumber;
-  do {
-    inputNumber = await readInput('数字を入力してください? ');
-    inputNumber = parseInt(inputNumber);
-  } while (Number.isNaN(inputNumber));
-  const results = [];
-  for (let i = 1; i <= inputNumber; i++) {
-    results.push(step1(String(i)));
-  }
-  console.log(results.join(', '));
-})();
