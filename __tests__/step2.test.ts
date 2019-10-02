abb8e0636bed1603eeba9f25d9e5cac8a4ad9963
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-10-02 16:30:52 +0900

    Step2:テスト拡充

--- __tests__/step2.test.ts
+++ __tests__/step2.test.ts
@@ -2,18 +2,38 @@ import { sum, average, max, min } from '../src/step2';
 
 const input = [20, 31, 42, 13, 5, 39];
 
-test('合計', () => {
-  expect(sum(input)).toBe(150);
+describe('正常系', () => {
+  test('合計', () => {
+    expect(sum(input)).toBe(150);
+  });
+
+  test('平均', () => {
+    expect(average(input)).toBe(25);
+  });
+
+  test('最大', () => {
+    expect(max(input)).toBe(42);
+  });
+
+  test('最小', () => {
+    expect(min(input)).toBe(5);
+  });
 });
 
-test('平均', () => {
-  expect(average(input)).toBe(25);
-});
+describe('異常系', () => {
+  test('合計', () => {
+    expect(sum([])).toBe(0);
+  });
 
-test('最大', () => {
-  expect(max(input)).toBe(42);
-});
+  test('平均', () => {
+    expect(average([])).toBe(0);
+  });
 
-test('最小', () => {
-  expect(min(input)).toBe(5);
+  test('最大', () => {
+    expect(max([])).toBe(null);
+  });
+
+  test('最小', () => {
+    expect(min([])).toBe(null);
+  });
 });
